8f6867ad4af2963e5b580314081662ba5a713491
ninhnguyen <xman@192.168.68.122>
2026-04-25 07:44:43 +0700

    fix cors for admin domain

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import 'dotenv/config';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import "dotenv/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3002",
+      "the1970-admin-5747vbo51-the1970-cos-projects.vercel.app",
+      "https://operations.the1970.co",
+    ],
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
