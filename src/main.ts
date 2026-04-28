5fdd46cfc247fa492d403d9e511cfd97bd24f0a4
ninhnguyen <xman@192.168.68.121>
2026-04-28 19:26:30 +0700

    fix cors origins

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3002",
-      "the1970-admin-5747vbo51-the1970-cos-projects.vercel.app",
-      "https://operations.the1970.co",
-    ],
-    credentials: true,
-  });
+app.enableCors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3002",
+    "https://the1970-admin-5747vbo51-the1970-cos-projects.vercel.app",
+    "https://operations.the1970.co",
+  ],
+  credentials: true,
+});
 
   await app.listen(process.env.PORT ?? 3001);
 }
