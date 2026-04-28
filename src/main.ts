1307c629a235e0e254f066ab84aced969c51576a
ninhnguyen <xman@192.168.68.121>
2026-04-28 20:51:11 +0700

    fix main.ts bootstrap + cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-app.enableCors({
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3002",
-    "https://the1970-admin-5747vbo51-the1970-cos-projects.vercel.app",
-    "https://operations.the1970.co",
-  ],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-});
+import "dotenv/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3002",
+      "https://the1970-admin-5747vbo51-the1970-cos-projects.vercel.app",
+      "https://operations.the1970.co",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
+}
+
+bootstrap();
